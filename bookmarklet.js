2cf7d7e14639046af5438a80f3cbdfd67639b74a
ngntrgduc <trungducnguyen1511@gmail.com>
2022-10-15 23:27:13 +0700

    Refactor code and fix bug

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,33 +2,35 @@ javascript: (() => {
     var tinchi = document.querySelectorAll('td:nth-child(3)');
     var monhoc = document.querySelectorAll('td:nth-child(2)');
     var diem = document.querySelectorAll('td:nth-child(6)');
-    var diemtren = 0, diemduoi = 0;
+    var tongdiem = 0, tongtinchi = 0;
     for (var i = 1; i < tinchi.length; i++) {
         if (
             monhoc[i].innerText.includes('Thể dục') ||
             monhoc[i].innerText.includes('Anh văn') ||
             monhoc[i].innerText.includes('Tin học cơ sở') ||
             monhoc[i].innerText.includes('Giáo dục quốc phòng') ||
+            !Number(diem[i].innerText) ||
             Number(diem[i].innerText) < 5
         ) {
             continue;
         }
-        diemtren += Number(tinchi[i].innerText) * Number(diem[i].innerText);
-        diemduoi += Number(tinchi[i].innerText);
+        tongdiem += Number(tinchi[i].innerText) * Number(diem[i].innerText);
+        tongtinchi += Number(tinchi[i].innerText);
     }
     var xeploai = '';
-    if (diemtren / diemduoi >= 9) {
+    var diemtrungbinh = tongdiem / tongtinchi;
+    if (diemtrungbinh >= 9) {
         xeploai = 'Xuất xắc';
-    } else if (diemtren / diemduoi >= 8 && diemtren / diemduoi < 9) {
+    } else if (diemtrungbinh >= 8 && diemtrungbinh < 9) {
         xeploai = 'Giỏi';
-    } else if (diemtren / diemduoi >= 7 && diemtren / diemduoi < 8) {
+    } else if (diemtrungbinh >= 7 && diemtrungbinh < 8) {
         xeploai = 'Khá';
-    } else if (diemtren / diemduoi >= 5 && diemtren / diemduoi < 7) {
+    } else if (diemtrungbinh >= 5 && diemtrungbinh < 7) {
         xeploai = 'Trung bình';
-    } else if (diemtren / diemduoi >= 4 && diemtren / diemduoi < 5) {
+    } else if (diemtrungbinh >= 4 && diemtrungbinh < 5) {
         xeploai = 'Yếu';
     } else {
         xeploai = 'Kém';
     }
-    alert('- Tổng số tín chỉ đã cày được : ' + diemduoi.toString() + '\n- Điểm trung bình của bạn : ' + (diemtren / diemduoi).toFixed(2).toString() + '\n- Xếp loại : ' + xeploai);
+    alert('- Tổng số tín chỉ đã cày được : ' + tongtinchi.toString() + '\n- Điểm trung bình của bạn : ' + (diemtrungbinh).toFixed(2).toString() + '\n- Xếp loại : ' + xeploai);
 })();
